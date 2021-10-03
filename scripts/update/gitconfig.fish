05164a0b256b7fe94e4628e895c51ad540743cf8
ytk6565 <ytk6565@gmail.com>
2021-10-03 23:30:06 +0900

    add gitconfig script

---
+++ scripts/update/gitconfig.fish
@@ -0,0 +1,3 @@
+sudo cp /Users/Shared/.gitconfig ./.shared.gitconfig
+sudo chmod -R 755 .shared.gitconfig
+sudo chown -R (whoami) .shared.gitconfig
--- scripts/update/index.fish
+++ scripts/update/index.fish
@@ -1,2 +1,3 @@
 fish scripts/update/Brewfile.fish
 fish scripts/update/fish.fish
+fish scripts/update/gitconfig.fish
