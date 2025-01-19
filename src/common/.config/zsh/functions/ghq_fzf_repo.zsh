7b4c3e212ca16b8a02c3241df0acc37db218c31e
ytk6565 <ytk6565@gmail.com>
2025-01-19 13:11:19 +0900

    add: ghq_fzf_repo for warp

--- src/common/.config/zsh/functions/ghq_fzf_repo.zsh
+++ src/common/.config/zsh/functions/ghq_fzf_repo.zsh
@@ -1,4 +1,6 @@
 # ghq + fzf
+
+# for Standard
 function ghq_fzf_repo() {
   local repo=$(ghq list | fzf --preview "ghq list --full-path --exact {} | xargs eza -h --long --icons --classify --git --no-permissions --no-user --no-filesize --git-ignore --sort modified --reverse --tree --level 2")
   if [ -n "$repo" ]; then
@@ -14,3 +16,11 @@ zle -N ghq_fzf_repo
 
 # key bindings
 bindkey '^g' ghq_fzf_repo
+
+# for Warp
+function g() {
+  local repo=$(ghq list | fzf --preview "ghq list --full-path --exact {} | xargs eza -h --long --icons --classify --git --no-permissions --no-user --no-filesize --git-ignore --sort modified --reverse --tree --level 2")
+  if [ -n "$repo" ]; then
+    cd $(ghq root)/$repo
+  fi
+}
